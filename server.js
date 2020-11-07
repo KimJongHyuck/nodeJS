386724ba369c22522d7bffcec1a9a85442af03c5
JongHyuckKim <kasiruske@naver.com>
2020-11-08 04:56:17 +0900

    404 예외처리 구현

--- server.js
+++ server.js
@@ -4,50 +4,42 @@ const url = require('url');
 const app = http.createServer(function(request,response){
     let _url = request.url;
     const queryData = url.parse(_url, true).query;
-    console.log(queryData.id);   
+    const pathname = url.parse(_url, true).pathname;
+    console.log(`쿼리 ㅏ이디:`+queryData.id);   
     let title = queryData.id;
-    
-    if(_url == '/'){
-    //   _url = 'index.html';
-    title = 'Welcome';
-      
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    const template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=javaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language
-   (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web 
-   browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. 
-   HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-  <img src="./coding.jpg" width="100%">
-  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, 
-  images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a 
-  means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes 
-  and other items. HTML elements are delineated by tags, written using angle brackets.
-  </p>
-</body>
-</html>
 
-    `;
-    response.end(template);
+    console.log(url.parse(_url, true)); // url 정보 보기 
+    if(pathname === '/'){
+    fs.readFile(`data/${queryData.id}`,'utf8', (err, description) => {
+        // if (err) throw err;
+        let template = `
+            <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=javaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}
+        </p>
+        </body>
+        </html>
+    
+        `;
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
 });
 app.listen(3000,()=>{
     console.log("the server is running on port 3000")
